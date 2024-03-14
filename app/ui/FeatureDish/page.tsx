04124c4e5cc73eab220f05df7bb134dcfc33fadb
Steven Wu <steven@Stevens-Air.local>
2024-03-13 23:13:52 -0500

    test

--- app/ui/FeatureDish/page.tsx
+++ app/ui/FeatureDish/page.tsx
@@ -7,22 +7,20 @@ interface FeaturedDish {
     moveY: string
 }
 
-const FeatureDish: React.FC<FeaturedDish> = ({ dishName, dishImage, moveX, moveY }): JSX.Element => {
+export default function FeatureDish(props: FeaturedDish): JSX.Element {
   return (
     <div className='group m-2 p-5 relative flex justify-center items-center overflow-hidden
       min-w-[300px] max-w-[500px] min-h-[600px] flex-auto'>
         <p className='text-2xl font-bold text-center tracking-widest text-wrap text-slate-50 absolute opacity-0 z-10
             transition-opacity ease-in-out duration-700
             group-hover:opacity-100 drop-shadow-md'>
-            { dishName }
+            { props.dishName }
         </p>
         
-        <img src={ dishImage } alt={ dishName }
+        <img src={ props.dishImage } alt={ props.dishName }
             className='absolute scale-90 max-w-[400%] h-auto m-auto ease-in-out duration-700
                 group-hover:blur-sm group-hover:scale-125'
-            style={{left: `${moveX}`, top: `${moveY}`}}></img>
+            style={{left: `${props.moveX}`, top: `${props.moveY}`}}></img>
     </div>
   )
 }
-
-export default FeatureDish;
