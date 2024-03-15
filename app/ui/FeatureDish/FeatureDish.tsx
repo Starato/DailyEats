69a6a6153e8f7a6c90722d1b34a7063a961bcbdd
Steven Wu <steven@mwc-084087.dhcp.missouri.edu>
2024-03-14 19:18:48 -0500

    fix resizing issues with featured dishes

--- app/ui/FeatureDish/FeatureDish.tsx
+++ app/ui/FeatureDish/FeatureDish.tsx
@@ -10,7 +10,7 @@ interface FeaturedDish {
 export function FeatureDish(props: FeaturedDish): JSX.Element {
   return (
     <div className='group m-2 p-5 relative flex justify-center items-center overflow-hidden
-      min-w-[300px] max-w-[500px] min-h-[600px] flex-auto'>
+      w-[150px] h-[500px] flex-auto'>
         <p className='text-2xl font-bold text-center tracking-widest text-wrap text-slate-50 absolute opacity-0 z-10
             transition-opacity ease-in-out duration-700
             group-hover:opacity-100 drop-shadow-md'>
@@ -18,7 +18,7 @@ export function FeatureDish(props: FeaturedDish): JSX.Element {
         </p>
         
         <img src={ props.dishImage } alt={ props.dishName }
-            className='absolute scale-90 max-w-[400%] h-auto m-auto ease-in-out duration-700
+            className='relative scale-90 min-w-max m-auto ease-in-out duration-700
                 group-hover:blur-sm group-hover:scale-125'
             style={{left: `${props.moveX}`, top: `${props.moveY}`}}></img>
     </div>
